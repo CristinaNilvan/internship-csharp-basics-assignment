19d1f3b83452800198335518597b2ec4ae4d8e0e
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-07-22 18:57:00 +0300

    Update Program

--- Project/Project/Program.cs
+++ Project/Project/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Project;
+
+Ingredient ingredient01 = new Ingredient(1, "rosie", 18, 40);
+Ingredient ingredient02 = new Meat(2, "pulpa", 50, 200, "pui");
+Ingredient ingredient03 = new Dairy(3, "lapte", 34, 30, 4);
+
+Console.WriteLine(ingredient01.GenerateIngredientCode());
+Console.WriteLine(ingredient02.GenerateIngredientCode());
+Console.WriteLine(ingredient03.GenerateIngredientCode());
