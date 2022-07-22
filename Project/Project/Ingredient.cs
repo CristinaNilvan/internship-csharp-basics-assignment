44c44c5bb4f4e3b68ac7c72644d548b45bfab9f4
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-07-22 18:15:01 +0300

    Add project

---
+++ Project/Project/Ingredient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    internal class Ingredient
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Type { get; set; }
+        public int Calories { get; set; }
+        public double Quantity { get; set; }
+
+        public Ingredient(int id, string? name, string? type, int calories, double quantity)
+        {
+            Id = id;
+            Name = name;
+            Type = type;
+            Calories = calories;
+            Quantity = quantity;
+        }
+    }
+}
