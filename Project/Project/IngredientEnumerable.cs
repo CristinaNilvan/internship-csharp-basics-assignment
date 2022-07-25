904649d06e86cbb46526daf52d02673a4ca4e9c0
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-07-25 13:10:44 +0300

    Create classes

---
+++ Project/Project/IngredientEnumerable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    internal class IngredientEnumerable : IEnumerable
+    {
+        private Ingredient[] _ingredients;
+
+        public IngredientEnumerable(Ingredient[] ingredients)
+        {
+            _ingredients = new Ingredient[ingredients.Length];
+            for (int i = 0; i < ingredients.Length; i++)
+            {
+                _ingredients[i] = ingredients[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator) GetEnumerator();
+        }
+
+        public IngredientEnumerator GetEnumerator()
+        {
+            return new IngredientEnumerator(_ingredients);
+        }
+    }
+}
---
+++ Project/Project/IngredientEnumerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    internal class IngredientEnumerator : IEnumerator
+    {
+        private int _position;
+        private Ingredient[] _ingredients;
+
+        public IngredientEnumerator(Ingredient[] ingredients)
+        {
+            _position = -1;
+            _ingredients = ingredients;
+        }
+
+        public Ingredient Current => _ingredients[_position];
+
+        object IEnumerator.Current => Current;
+
+        public bool MoveNext()
+        {
+            _position++;
+            return _position < _ingredients.Length;
+        }
+
+        public void Reset()
+        {
+            _position = -1;
+        }
+    }
+}
